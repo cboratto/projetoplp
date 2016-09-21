b506c7ddc6787fba04d7bb971c63b22ccfdbb91c
Caio Boratto <caioboratto@brtt.local>
2016-09-20 21:29:06 -0300

    TODO: Na escrita do token, ele esta considerando o token anterior, mas nao o correto. Um exemplo e o token class.

--- lexico.c
+++ lexico.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-char str[1000];
+char str[2048];
 static const char TOKEN_FILE[] = "filew.txt";
 
 int main() {
@@ -77,156 +77,172 @@ void tokenLibrary(char * tk) {
 	// PALAVRAS RESERVADAS
 	//
 	if (strcmp("if",tk)==0) {
-		printf("RECONHECIDO: %s \n", tk );
-		write(tk);
+		//printf("RECONHECIDO: %s \n", tk );
+		write("<if>");
 	} else if (strcmp("while",tk)==0) {
-		printf("RECONHECIDO: %s \n", tk );
-		write(tk);
+		//printf("RECONHECIDO: %s \n", tk );
+		write("<while>");
 	} else if (strcmp("this",tk)==0) {
-		printf("RECONHECIDO: %s \n", tk );
-		write(tk);
+		//printf("RECONHECIDO: %s \n", tk );
+		write("<this>");
 	} else if (strcmp("new",tk)==0) {
-		printf("RECONHECIDO: %s \n", tk );
-		write(tk);
+		//printf("RECONHECIDO: %s \n", tk );
+		write("<new>");
 	} else if (strcmp("else",tk)==0) {
-		printf("RECONHECIDO: %s \n", tk );
-		write(tk);
+		//printf("RECONHECIDO: %s \n", tk );
+		write("<else>");
 	} else if (strcmp("System.out.println",tk)==0) {
-		printf("RECONHECIDO: %s \n", tk );
-		write(tk);
+		//printf("RECONHECIDO: %s \n", tk );
+		write("<System.out.println>");
 	} else if (strcmp("boolean",tk)==0) {
-		printf("RECONHECIDO: %s \n", tk );
-		write(tk);
+		//printf("RECONHECIDO: %s \n", tk );
+		write("<boolean>");
 	} else if (strcmp("class",tk)==0) {
-		printf("RECONHECIDO: %s \n", tk );
-		write(tk);
+		//printf("RECONHECIDO: %s \n", tk );
+		write("<class>");
 	} else if (strcmp("extends",tk)==0) {
-		printf("RECONHECIDO: %s \n", tk );
-		write(tk);
+		//printf("RECONHECIDO: %s \n", tk );
+		write("<class>");
 	} else if (strcmp("public",tk)==0) {
-		printf("RECONHECIDO: %s \n", tk );
-		write(tk);
+		//printf("RECONHECIDO: %s \n", tk );
+		write("<public>");
 	} else if (strcmp("static",tk)==0) {
-		printf("RECONHECIDO: %s \n", tk );
-		write(tk);
+		//printf("RECONHECIDO: %s \n", tk );
+		write("<static>");
 	} else if (strcmp("void",tk)==0) {
-		printf("RECONHECIDO: %s \n", tk );
-		write(tk);
+		//printf("RECONHECIDO: %s \n", tk );
+		write("<void>");
 	} else if (strcmp("main",tk)==0) {
-		printf("RECONHECIDO: %s \n", tk );
-		write(tk);
+		//printf("RECONHECIDO: %s \n", tk );
+		write("<main>");
 	} else if (strcmp("return",tk)==0) {
-		printf("RECONHECIDO: %s \n", tk );
-		write(tk);
+		//printf("RECONHECIDO: %s \n", tk );
+		write("<return>");
 	} else if (strcmp("int",tk)==0) {
-		printf("RECONHECIDO: %s \n", tk );
-		write(tk);
+		//printf("RECONHECIDO: %s \n", tk );
+		write("<int>");
 	}
 	//
 	// PONTUACAO
 	//
 	else if (strcmp("(",tk)==0) {
-		printf("RECONHECIDO: %s \n", tk );
-		write(tk);		
+		//printf("RECONHECIDO: %s \n", tk );
+		write("<AP>");		
 	} else if (strcmp(")",tk)==0) {
-		printf("RECONHECIDO: %s \n", tk );
-		write(tk);		
+		//printf("RECONHECIDO: %s \n", tk );
+		write("<FP>");		
 	} else if (strcmp("[",tk)==0) {
-		printf("RECONHECIDO: %s \n", tk );
-		write(tk);		
+		//printf("RECONHECIDO: %s \n", tk );
+		write("<AC>");		
 	} else if (strcmp("]",tk)==0) {
-		printf("RECONHECIDO: %s \n", tk );
-		write(tk);		
+		//printf("RECONHECIDO: %s \n", tk );
+		write("<FC>");		
 	} else if (strcmp("{",tk)==0) {
-		printf("RECONHECIDO: %s \n", tk );
-		write(tk);		
+		//printf("RECONHECIDO: %s \n", tk );
+		write("<ACH>");		
 	} else if (strcmp("}",tk)==0) {
 		printf("RECONHECIDO: %s \n", tk );
-		write(tk);		
+		write("<FCH>");		
 	} else if (strcmp(".",tk)==0) {
-		printf("RECONHECIDO: %s \n", tk );
-		write(tk);		
+		//printf("RECONHECIDO: %s \n", tk );
+		write("<.>");		
 	} else if (strcmp(",",tk)==0) {
-		printf("RECONHECIDO: %s \n", tk );
-		write(tk);		
+		//printf("RECONHECIDO: %s \n", tk );
+		write("<,>");		
 	}
 	//
 	// OPERADORES
 	//
 	else if (strcmp("==",tk)==0) {
-		printf("RECONHECIDO: %s \n", tk );
-		write(tk);		
+		//printf("RECONHECIDO: %s \n", tk );
+		write("<OP,EQ>");		
 	} else if (strcmp("&&",tk)==0) {
-		printf("RECONHECIDO: %s \n", tk );
-		write(tk);		
+		//printf("RECONHECIDO: %s \n", tk );
+		write("<OP,AND>");
 	} else if (strcmp("-",tk)==0) {
-		printf("RECONHECIDO: %s \n", tk );
-		write(tk);		
+		//printf("RECONHECIDO: %s \n", tk );
+		write("<OP,MINUS>");		
 	} else if (strcmp("+",tk)==0) {
-		printf("RECONHECIDO: %s \n", tk );
-		write(tk);		
+		//printf("RECONHECIDO: %s \n", tk );
+		write("<OP,PLUS>");		
 	} else if (strcmp("*",tk)==0) {
-		printf("RECONHECIDO: %s \n", tk );
-		write(tk);		
+		//printf("RECONHECIDO: %s \n", tk );
+		write("<OP,MULT>");		
 	} else if (strcmp("!",tk)==0) {
-		printf("RECONHECIDO: %s \n", tk );
-		write(tk);		
+		//printf("RECONHECIDO: %s \n", tk );
+		write("<OP,NOT>");		
 	} else if (strcmp("=",tk)==0) {
-		printf("RECONHECIDO: %s \n", tk );
-		write(tk);		
+		//printf("RECONHECIDO: %s \n", tk );
+		write("<OP,ATR>");		
 	} else if (strcmp("!=",tk)==0) {
-		printf("RECONHECIDO: %s \n", tk );
-		write(tk);		
+		//printf("RECONHECIDO: %s \n", tk );
+		write("<OP,DIF>");		
 	} else if (strcmp("<",tk)==0) {
-		printf("RECONHECIDO: %s \n", tk );
-		write(tk);		
-	} 
-	//
-	// NUMEROS
-	//	
-	int i;
-	char number[1024];
-	int index=0;
-
-	for (i=0; tk[i]!='\n'; i++){		
-		if (tk[i]=='0') {	
-			number[index++]=tk[i];
-
-		} else if (tk[i]=='1') {
-			number[index++]=tk[i];
-
-		} else if (tk[i]=='2') {
-			number[index++]=tk[i];
-
-		} else if (tk[i]=='3') {
-			number[index++]=tk[i];
-
-		} else if (tk[i]=='4') {
-			number[index++]=tk[i];
-
-		} else if (tk[i]=='5') {
-			number[index++]=tk[i];
-
-		} else if (tk[i]=='6') {
-			number[index++]=tk[i];
-
-		} else if (tk[i]=='7') {
-			number[index++]=tk[i];
-
-		} else if (tk[i]=='8') {
-			number[index++]=tk[i];
-
-		} else if (tk[i]=='9') {
-			number[index++]=tk[i];
-		} else {
-			break;
-		}
+		//printf("RECONHECIDO: %s \n", tk );
+		write("<OP,LT>");		
 	}
-	//if (isNumber==1) {
-		//number[index++]='\n';
-		printf("numero reconhecido %s\n",number);		
-	//}
+        //
+        // NUMEROS
+        //  
+        int i;
+        char number[1024];
+        //Limpa area de memoria. Necessario para concatenacao de Numero realizada mais abaixo
+        memset(number,0, 1024);
+        int index=0;
+        int totalTimesCountNumber=0;
+        int totalTimesIterate=0;
+        
 
 
+        for (i=0; tk[i]!='\0'; i++){        
+            if (tk[i]=='0') {   
+                number[index++]=tk[i];
+                totalTimesCountNumber++;
 
+            } else if (tk[i]=='1') {
+                number[index++]=tk[i];
+                totalTimesCountNumber++;
+
+            } else if (tk[i]=='2') {
+                number[index++]=tk[i];
+                totalTimesCountNumber++;
+
+            } else if (tk[i]=='3') {
+                number[index++]=tk[i];
+                totalTimesCountNumber++;
+
+            } else if (tk[i]=='4') {
+                number[index++]=tk[i];
+                totalTimesCountNumber++;
+
+            } else if (tk[i]=='5') {
+                number[index++]=tk[i];
+                totalTimesCountNumber++;
+
+            } else if (tk[i]=='6') {
+                number[index++]=tk[i];
+                totalTimesCountNumber++;
+
+            } else if (tk[i]=='7') {
+                number[index++]=tk[i];
+                totalTimesCountNumber++;
+
+            } else if (tk[i]=='8') {
+                number[index++]=tk[i];
+                totalTimesCountNumber++;
+
+            } else if (tk[i]=='9') {
+                number[index++]=tk[i];
+                totalTimesCountNumber++;
+            } 
+            totalTimesIterate++;
+        }
+
+        if (totalTimesCountNumber==totalTimesIterate) {
+            char n[1024];
+            strcpy(n, "<num,");
+            strcat(n, number);
+            strcat(n, ">"); 
+            write(n); 
+        }
 }
